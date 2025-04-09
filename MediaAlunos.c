1ab451204abbd5eb95a837f42ba0b2570964af63
João Paulo Fernandes <103535492+jpfernandes1@users.noreply.github.com>
2025-04-09 11:44:58 +0000

    Criação programa calcula média

--- MediaAlunos.c
+++ MediaAlunos.c
@@ -1,1 +1,22 @@
+/* Crie um programa em C que calcula a média de três notas inseridas pelo usuário. 
+Utilize variáveis para armazenar as notas, operadores matemáticos para somar e calcular a média, 
+e operadores de atribuição para atualizar valores. */
 
+# include <stdio.h>
+
+int main(){
+    int nota1, nota2, nota3;
+
+    printf("Calculadora de Médias\n");
+    printf("Digite a primeira nota:");
+    scanf("%d", &nota1);
+    printf("Digite a segunda nota:");
+    scanf("%d", &nota2);
+    printf("Digite a terceira nota:");
+    scanf("%d", &nota3);
+
+    float media = (nota1 + nota2 + nota3)/3;
+
+    printf("A média das notas inseridas é: %.2f", media);
+
+}
