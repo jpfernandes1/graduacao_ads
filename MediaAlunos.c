469db9fc70f5342ba06fbfe23192cec4e614b0fa
João Paulo Fernandes <103535492+jpfernandes1@users.noreply.github.com>
2025-04-09 16:48:08 +0000

    SuperTrunfo nível novato em funcionamento

--- MediaAlunos.c
+++ MediaAlunos.c
@@ -5,15 +5,15 @@ e operadores de atribuição para atualizar valores. */
 # include <stdio.h>
 
 int main(){
-    int nota1, nota2, nota3;
+    float nota1, nota2, nota3;
 
     printf("Calculadora de Médias\n");
     printf("Digite a primeira nota:");
-    scanf("%d", &nota1);
+    scanf("%f", &nota1);
     printf("Digite a segunda nota:");
-    scanf("%d", &nota2);
+    scanf("%f", &nota2);
     printf("Digite a terceira nota:");
-    scanf("%d", &nota3);
+    scanf("%f", &nota3);
 
     float media = (nota1 + nota2 + nota3)/3;
 
--- super_trunfo.c
+++ super_trunfo.c
@@ -1,1 +1,78 @@
+# include <stdio.h>
 
+int main(){
+
+    char Estado1[20];
+    char CodigoCarta1[20];
+    char NomeCidade1[20];
+    int Populacao1;
+    float Area1;
+    float PIB1;
+    int QntPontosTuristicos1;
+
+    char Estado2[20];
+    char CodigoCarta2[20];
+    char NomeCidade2[20];
+    int Populacao2;
+    float Area2;
+    float PIB2;
+    int QntPontosTuristicos2;
+
+printf("---Cadastro de Cartas para o Super Trunfo---\n\n");
+printf("Carta 1:\n\n");
+printf("Digite a letra do Estado (de 'A' a 'H'):");
+scanf("%s", &Estado1);
+printf("Digite o código da Carta (um número de 01 a 04):");
+scanf("%s", &CodigoCarta1);
+printf("Digite o nome da cidade:");
+scanf("%s", &NomeCidade1);
+printf("Digite a população da cidade: ");
+scanf("%d", &Populacao1);
+printf("Digite a área da cidade (em km²):");
+scanf("%f", &Area1);
+printf("Digite o PIB da cidade: ");
+scanf("%f", &PIB1);
+printf("Digite o numero de pontos turísticos da cidade: ");
+scanf("%d", &QntPontosTuristicos1);
+
+
+printf("\n\nCarta 2:\n\n");
+printf("Digite a letra do Estado (de 'A' a 'H'):");
+scanf("%s", &Estado2);
+printf("Digite o código da Carta (um número de 01 a 04):");
+scanf("%s", &CodigoCarta2);
+printf("Digite o nome da cidade:");
+scanf("%s", &NomeCidade2);
+printf("Digite a população da cidade: ");
+scanf("%d", &Populacao2);
+printf("Digite a área da cidade (em km²):");
+scanf("%f", &Area2);
+printf("Digite o PIB da cidade: ");
+scanf("%f", &PIB2);
+printf("Digite o numero de pontos turísticos da cidade: ");
+scanf("%d", &QntPontosTuristicos2);
+
+printf("\n -------Seguem os dados cadastrados:------\n");
+
+printf("\nCarta 01:\n");
+printf("\nEstado: %s", Estado1);
+printf("\nCódigo: %s%s", Estado1, CodigoCarta1);
+printf("\nNome da Cidade: %s", NomeCidade1);
+printf("\nPopulação: %d", Populacao1);
+printf("\nÁrea: %.2f", Area1, "km²");
+printf("\nPIB: %.2f", PIB1, "bilhões de reais");
+printf("\nNúmero de Pontos Turísticos: %d\n", QntPontosTuristicos1);
+
+printf("\n\nCarta 02:\n");
+printf("\nEstado: %s", Estado2);
+printf("\nCódigo: %s%s", Estado2, CodigoCarta2);
+printf("\nNome da Cidade: %s", NomeCidade2);
+printf("\nPopulação: %d", Populacao2);
+printf("\nÁrea: %.2f", Area2, "km²");
+printf("\nPIB: %.2f", PIB2, "bilhões de reais");
+printf("\nNúmero de Pontos Turísticos: %d", QntPontosTuristicos2);
+
+
+
+printf("\n Fim do programa---");
+}
