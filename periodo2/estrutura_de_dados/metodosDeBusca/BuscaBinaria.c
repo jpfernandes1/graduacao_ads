912a0268701fc4560ac39673674c4e4c58960193
jpfernandes1 <jp_fernandes1@live.com>
2025-10-14 17:15:34 -0300

    implantação busca binaria recursiva

--- periodo2/estrutura_de_dados/metodosDeBusca/BuscaBinaria.c
+++ periodo2/estrutura_de_dados/metodosDeBusca/BuscaBinaria.c
@@ -1,4 +1,4 @@
-/* Descrição: Procura por um valor em um vetor.
+/* Descrição: Procura por um valor em um vetor de forma iterativa
  Pré-requisitos: 
     - o vetor deve ser : 
         a) ORDENADO em ordem crescente
@@ -8,7 +8,7 @@
         - 1 se o vetor não contém o valor procurado
  */
 
- int BuscaBinaria(int vetor[], int tamanho, int valor){
+ int BuscaBinariaIterativa(int vetor[], int tamanho, int valor){
     int inicio = 0;
     int fim = tamanho - 1;
 
@@ -39,5 +39,38 @@
 
     return -1;
 
+}
 
- }
+/*
+    Descrição: procura por um valor em uma fatira de um vetor ordenado (definido por 'inicio' e 'fim') de forma recursiva.
+    Pré-requisito: idem anterior
+    Retorno: idem anterior
+*/
+
+int buscaBinariaRecursiva(int vetor[], int inicio, int fim, int valor){
+    // CASO BASE: Se a área de busca se torna inválida (inicio>fim),
+    // o elemento não existe na lista. O trabalho da recursão para aqui:
+    if (inicio > fim){
+        return -1;
+    }
+
+    // Calcula a posição do meio de forma segura contra overflow:
+    int meio = inicio + (fim - inicio)/2;
+
+    // Se o elemento do meio é o que procuramos, retorna o indice dele:
+    if (vetor[meio] == valor){
+        return meio;
+    }
+
+    // Recursividade (delegação):
+    // Se o elemento do meio for menor que o valor, delegamos a busca para
+    // a metade direita do vetor:
+    else if (vetor[meio] < valor){
+        return buscaBinariaRecursiva(vetor, meio + 1, fim, valor);
+    }
+    // Caso contrario, delegamos para a metade esquerda:
+    else {
+        return buscaBinariaRecursiva(vetor, inicio, meio - 1, valor);
+    }
+    
+}
