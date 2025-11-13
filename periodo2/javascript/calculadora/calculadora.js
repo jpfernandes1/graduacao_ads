3f8c21661cb863e777c51e476351a6f0fd7aca70
jpfernandes1 <jp_fernandes1@live.com>
2025-11-13 15:00:03 -0300

    implementação calculadora;

---
+++ periodo2/javascript/calculadora/calculadora.js
@@ -0,0 +1,9 @@
+function somar(a,b) {return a+b;}
+function subtrair(a,b) {return a-b;}
+function multiplicar(a,b) {return a*b;}
+
+function dividir(a,b){
+    if(b==0){
+        return alert("Divisão por zero!")
+    }
+}
